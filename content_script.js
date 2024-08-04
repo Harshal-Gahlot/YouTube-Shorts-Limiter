8a3ce19c12fac89753c4b1fded8d270be5be3bb8
Harshal Gahlot <gahlotharshal@gmail.com>
2024-08-05 01:26:20 +0530

    Now extension work even when we go from a YT vid to shorts. Insted of directly going to youtube.com.shorts/...

--- content_script.js
+++ content_script.js
@@ -1,20 +1,28 @@
-storage = new Set()
-SHORTS_LIM = 3
-time = 0
-var timerId = setInterval(() => {
-    const pathName = window.location.pathname.slice(8);
-    storage.add(pathName);
-    console.log(storage.size, storage, time);
-    time += 1
-    if (time >= 180 || storage.size > SHORTS_LIM) {
-        clearInterval(timerId);
-        console.log('size out');
-        short = document.querySelector("#page-manager");
-        short.innerHTML = "<H1 style='margin: auto auto; background-color:#f00; padding:20px'>ENOUGH SHORTS FOR NOW, GO BACK TO WORK BITCH !</H1>";
-        short.style.position = "absolute";
-        short.style.height = '80vh';
-        short.style.width = '75vw';
-        short.style.disply = 'grid';
-        short.style.placeItems = 'center';
-    }
-}, 1000);
+function core() {
+    if (window.location.pathname.slice(1, 7) === "shorts") {
+        storage = new Set()
+        SHORTS_LIM = 3
+        TIME = SHORTS_LIM * 50
+        time = 0
+        var timerId = setInterval(() => {
+            const pathName = window.location.pathname.slice(8);
+            storage.add(pathName);
+            console.log(storage.size, storage, time);
+            time += 1
+            if (time >= TIME || storage.size > SHORTS_LIM) {
+                clearInterval(timerId);
+                console.log('size out');
+                short = document.querySelector("#page-manager");
+                short.innerHTML = "<H1 style='margin: auto auto; background-color:#f00; padding:20px'>ENOUGH SHORTS FOR NOW, GO BACK TO WORK BITCH !</H1>";
+                short.style.position = "absolute";
+                short.style.height = '80vh';
+                short.style.width = '75vw';
+                short.style.disply = 'grid';
+                short.style.placeItems = 'center';
+                setTimeout(core, 1000);
+            }
+        }, 1000);
+    } else { setTimeout(core, 1000); }
+}
+
+core()
