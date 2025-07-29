bec3486d3953fb3ce6cfe79dbff4b13c90753025
Harshal Gahlot <gahlotharshal@gmail.com>
2025-07-29 11:13:31 +0530

    added daily reset at 4AM

--- content_script.js
+++ content_script.js
@@ -3,8 +3,8 @@ function core() {
     if (location.pathname.startsWith('/shorts/')) {
         console.log('YouTube Shorts detected\n\n');
         chrome.storage.local.get(
-            ['total_shorts_of_day', 'shorts_spree', 'total_shorts_watch_time', 'SHORTS_LIM', 'TIME_LIM', 'reset_in'],
-            ({ total_shorts_of_day = 0, shorts_spree = 0, total_shorts_watch_time = 0, SHORTS_LIM = 3, TIME_LIM = 60, reset_in = 0 }) => {
+            ['total_shorts_of_day', 'shorts_spree', 'total_shorts_watch_time', 'SHORTS_LIM', 'TIME_LIM', 'reset_in', 'daily_reset_in'],
+            ({ total_shorts_of_day = 0, shorts_spree = 0, total_shorts_watch_time = 0, SHORTS_LIM = 3, TIME_LIM = 60, reset_in = 0, daily_reset_in = 0 }) => {
 
                 console.log("TOP FIRST PRINTING ALL VALUES::::::::", 'total_shorts_of_day', total_shorts_of_day, 'shorts_spree', shorts_spree, 'total_shorts_watch_time', total_shorts_watch_time, 'SHORTS_LIM', SHORTS_LIM, 'TIME_LIM', TIME_LIM, 'reset_in', reset_in, "Current time: ", Date.now());
 
@@ -52,6 +52,31 @@ function core() {
                             TIME_LIM
                         });
                         console.log('First short watched, setting reset timer:', Date.now() + 1000 * 60 * 60);
+
+                        if (daily_reset_in < Date.now()) {
+                            const date = new Date();
+                            const hours_passed = date.getHours();
+                            const min_passed = date.getMinutes();
+                            const seconds_passed = date.getSeconds();
+
+                            const seconds_left = (60 - seconds_passed) * 1000;
+                            const min_left = (59 - min_passed) * 60000; 
+                            let hours_left;
+                            if (hours_passed < 4) {
+                                hours_left = (3 - hours_passed) * 3600000; 
+                            } else {
+                                hours_left = (23 - hours_passed + 4) * 3600000;
+                            }
+                            const daily_reset = Date.now() + seconds_left + min_left + hours_left;
+
+                            chrome.storage.local.set({
+                                daily_reset_in: daily_reset
+                            });
+
+                            console.log("DAILY_RESET_IN:", new Date(daily_reset).toString());
+
+                        }
+
                     }
 
                     chrome.storage.local.set({
